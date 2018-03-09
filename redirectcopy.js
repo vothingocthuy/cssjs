26f2e93beca24b180dc897e0bda6a977ad00c263
vothingocthuy <37179743+vothingocthuy@users.noreply.github.com>
2018-03-09 16:26:34 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='thuy xinh dep 134456'>thuy xinh dep 134456"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/> vui lòng liên hệ trang : <a href='https://datnenkhunam.blogspot.com/'>https://datnenkhunam.blogspot.com/"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
